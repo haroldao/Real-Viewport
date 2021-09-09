76c622aefd1d9980b1f21aa54c0f90b772599a7b
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-09-09 13:24:17 +0200

    Update real-viewport.js

--- real-viewport.js
+++ real-viewport.js
@@ -7,7 +7,6 @@ const calc = () => {
   document.documentElement.style.setProperty('--vh', ${vh}px);
 
   let vw = window.innerWidth * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
   document.documentElement.style.setProperty('--vw', ${vw}px);
 }
 
@@ -17,6 +16,5 @@ window.setTimeout(() => {
 
 // Resize
 window.addEventListener('resize', () => {
-  // We execute the same script as before
   calc()
 });
